06d780673f50e0bf4f8f2696dbb821776fcdd4af
Artem Sorokin <Artem.Sorokin@vuoksi.fi>
2024-01-18 13:41:35 +0200

    Added particles to the player's weapon
    TODO: better particles

--- Assets/Scripts/WeaponHitbox.cs
+++ Assets/Scripts/WeaponHitbox.cs
@@ -4,6 +4,8 @@ public class WeaponHitbox : MonoBehaviour
 {
     [SerializeField]
     private int _damage;
+    [SerializeField]
+    private ParticleSystem _hitParticles;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -11,6 +13,8 @@ public class WeaponHitbox : MonoBehaviour
         {
             var enemyScript = other.gameObject.GetComponent<Enemy>();
             enemyScript.Damage(_damage);
+            if (_hitParticles.isPlaying) _hitParticles.Stop();
+            _hitParticles.Play();
         }
     }
 }
